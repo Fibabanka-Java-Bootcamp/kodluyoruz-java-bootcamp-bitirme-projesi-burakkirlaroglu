140a8b07ce6070caaa15c61cf45a427d1b3e1083
Burak Kırlaroğlu <52127496+burakkirlaroglu@users.noreply.github.com>
2021-01-30 02:00:33 +0300

    General fixes done and config file added !

--- src/main/java/org/kodluyoruz/mybank/card/CardService.java
+++ src/main/java/org/kodluyoruz/mybank/card/CardService.java
@@ -1,7 +1,23 @@
 package org.kodluyoruz.mybank.card;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CardService {
+
+    private final CardRepository cardRepository;
+
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public Card create(Card card){
+        return cardRepository.save(card);
+    }
+
+    public Page<Card> list(Pageable pageable){
+        return cardRepository.findAll(pageable);
+    }
 }
