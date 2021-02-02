ea57b775f7aaba98388208cafc94d53fddc627d5
Burak Kırlaroğlu <52127496+burakkirlaroglu@users.noreply.github.com>
2021-02-02 12:59:38 +0300

    Delete src/main/java/org/kodluyoruz/mybank/transfer directory

--- src/main/java/org/kodluyoruz/mybank/transfer/TransferRepository.java
+++
@@ -1,6 +0,0 @@
-package org.kodluyoruz.mybank.transfer;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TransferRepository extends CrudRepository<Transfer, Integer> {
-}
